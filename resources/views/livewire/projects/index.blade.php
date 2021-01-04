1fbe860a027f907ac4b207b5015627ecf22cdc9b
fahadxt <f.s.almotery@gmail.com>
2021-01-04 23:41:37 +0300

    project index front end " card "

--- resources/views/livewire/projects/index.blade.php
+++ resources/views/livewire/projects/index.blade.php
@@ -2,24 +2,59 @@
 
     @foreach ($data as $key => $value)
     <div class="w-full sm:w-1/1 md:w-1/2 lg:w-1/3 xl:w-1/3 2xl:w-1/4  p-3 ">
-        <div class="bg-white overflow-hidden flex-1 flex flex-col cursor-pointer 
-        hover:shadow-sm group block rounded-md border border-gray-200"
+        <div class="bg-white shadow-sm overflow-hidden flex-1 flex flex-col cursor-pointer 
+        hover:shadow-lg group block rounded-md font-semibold transform hover:scale-105 motion-reduce:transform-none"
             wire:click="show({{ $value->id }})">
 
 
-            <div class="flex justify-between p-3 bg-gradient-to-br from-blue-300 to-blue-500 text-white ">
+            <div class="flex justify-between p-3 bg-gradient-to-br from-blue-400 to-blue-500 text-white ">
                 <span  class="inline-block align-middle">{{$value->name}}</span >
-                <span  class="inline-block align-middle">{{$value->created_at->format('Y-M-d')}}</span >
+                <span  class="inline-block align-middle">
+                    {{$value->tasks->count()}}
+                    <i class="fas fa-tasks"></i>
+                </span >
             </div>
 
             <div class="p-4 flex-1 flex flex-col ">
-                <div class="mb-4 text-grey-darker text-sm flex-1 text-right">
-                    <p>{{$value->name}}</p>
+                <div class="flex relative">
+                    <div class="circle-icon-2">
+                        <i class="fas fa-file-alt"></i>
+                    </div>
+
+                    <div class="absolute inset-y-0.5 right-6 after-icon ">
+                        {{__('description')}}
+                    </div>
                 </div>
-                <div
-                    class="border-t border-grey-light pt-2 text-xs text-grey hover:text-red uppercase no-underline tracking-wide text-right">
-                    Twitter
+            
+                <div class="flex flex-wrap mt-5 text-right pr-5 text-gray-500 ">
+                    <p class=""> {{$value->description}} </p>
                 </div>
+
+                
+            </div>
+            <div class="flex justify-between p-3 footer-shadow pt-2 text-grey">
+                
+                <span  class="inline-block align-middle ">
+                    <!-- Component Start -->
+                    <div class="flex">
+                        @foreach ($value->users as $key => $user)
+                        {{-- <img src="{{ asset( $user->profile_photo_url ) }}" class="rounded-full max-w-full mx-auto" style="max-width:120px"> --}}
+                        <img class="w-8 h-8 rounded-full border-2 border-white {{$key > 0 ? '-mr-3' : ''}}" src="{{ asset( $user->profile_photo_url ) }}" alt="">  
+                        @if (($key+1) == 2)
+                            @break
+                        @endif
+                        @endforeach
+                        @if ($value->users->count() > 2)
+                            <span class="flex items-center justify-center font-semibold text-gray-600 text-xs w-8 h-8 rounded-full bg-gray-200 border-2 border-white -mr-3">+{{$value->users->count() -2 }}</span>
+                        @endif
+                    </div>
+                    <!-- Component End  -->
+                </span>
+
+                <span  class="inline-block align-middle text-gray-400 font-light" dir="ltr">
+                    <i class="far fa-calendar-alt"></i>
+                    {{$value->created_at->format('Y-M-d')}}
+                </span >
             </div>
         </div>
     </div>
